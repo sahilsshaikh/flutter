31ff14a899eee4db729ec3f71c719e9680228f91
sahilsshaikh <108098200+sahilsshaikh@users.noreply.github.com>
2023-08-04 16:06:11 +0530

    day 6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
   void main(){
@@ -15,19 +16,19 @@ import 'package:google_fonts/google_fonts.dart';
   Widget build(BuildContext context) {
      return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
+      theme:  ThemeData(
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
-        
       ),
+      debugShowCheckedModeBanner:false ,
       darkTheme: ThemeData(
         brightness: Brightness.dark 
       ),
       
       routes:{
         "/":(context) => LoginPage(),
-        "/home":(context) => HomePage(),
-        "/login":(context) => LoginPage(),
+        MyRoutes.homeRoute:(context) => HomePage(),
+        MyRoutes.loginRoute:(context) => LoginPage(),
         
       },
    );
